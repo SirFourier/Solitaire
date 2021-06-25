1b2c6f383784eee368b15e9f2e1c86eee6e6c7eb
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2021-06-25 23:23:41 +0100

    testing pile mechanics

--- Solitaire.py
+++ Solitaire.py
@@ -1,15 +1,17 @@
 # Solitaire Game
-# Source of card images: https://code.google.com/archive/p/vector-playing-cards/
+# Source of card images: https://code.google.com/archive/p/vector-playing-deck/
 import sys
 import pygame
+import random
 
 # initialise pygame
 pygame.init()
 
 # load and scale image to new size
-def loadImage(path, size):
+def loadImage(path, newSize=None):
     image = pygame.image.load(path)
-    image = pygame.transform.scale(image, size)
+    if newSize:
+        image = pygame.transform.scale(image, newSize)
     return image
 
 # card class containing image, position, and game data
@@ -26,7 +28,7 @@ class Card:
         self.number = number
         self.suit = suit
         self.image = image
-        self.rect = self.image.get_rect()
+        self.rect = image.get_rect()
         self.faceUp = True
         self.draggable = True
 
@@ -41,7 +43,7 @@ class Card:
         elif mouseY < self.rect.y or mouseY > self.rect.y + self.rect.h: 
             isInside = False
 
-        # set held is true if inside
+        # set held card to this card if cursor is inside
         if isInside: 
             Card.held = self
             Card.prevMouseX = mouseX
@@ -56,49 +58,69 @@ class Card:
         Card.prevMouseX = mouseX
         Card.prevMouseY = mouseY 
 
-    def handleMouseUp(self, pile):
-        # check the last card of each pile
-        # if near a valid card, attach to it
-        if self.rect.x < pile[-2].rect.x + 0.5 * pile[-2].rect.w and self.rect.x > pile[-2].rect.x - 0.5 * pile[-2].rect.w: 
-            if self.rect.y < pile[-2].rect.y + 0.5 * pile[-2].rect.h and self.rect.y > pile[-2].rect.y - 0.5 * pile[-2].rect.h: 
-                self.rect.x = pile[-2].rect.x
-                self.rect.y = pile[-2].rect.y + 40
-
+    def handleMouseUp(self):
         Card.held = None
 
     def draw(self, screen):
         # draws image onto rect surface
         screen.blit(self.image, self.rect)
 
+
+class Pile:
+
+    def __init__(self):
+        pass
+
+
+# set screen properties
+screenSize = width, height = 1100, 800
+darkGreen = 50, 122, 14 # for background
+
 # set card properties
-size = width, height = 1300, 750
 cardSize = width, height = 95, 140
-darkGreen = 50, 122, 14
 imagePath = "PNG-cards"
 suits = ("clubs", "diamonds", "hearts", "spades")
-cards = []
+deck = []
 
-# create cards
+# create deck
 for suit in suits:
     # 1 = ace, 11 = jack, 12 = queen, 13 = king
     for number in range(1, 14):
         image = loadImage(f"{imagePath}/{number}_of_{suit}.png", cardSize)
-        cards.append(Card(number, suit, image))
+        deck.append(Card(number, suit, image))
 
 # set cardback image
 Card.cardback = loadImage(f"{imagePath}/cardback.png", cardSize)
 
+# shuffle deck
+random.shuffle(deck)
+
+# remove cards from deck and add to piles
+
+# create 7 piles
+cardSpacing = 36
+pileSpacing = 120
+piles = []
+startingCardPosition = [100, 100]
+cardPosition = startingCardPosition.copy()
+for _ in range(7):
+    pile = []
+    for _ in range(5):
+        card = deck.pop()
+        card.rect.x, card.rect.y = cardPosition
+        pile.append(card)
+        # move position of next card in the y direction
+        cardPosition[1] += cardSpacing 
+    
+    piles.append(pile)
+    # move position of next pile in the x direction
+    cardPosition[0] += pileSpacing
+    # reset y position of pile
+    cardPosition[1] = startingCardPosition[1]
+
 # set screen and clock
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(screenSize)
 clock = pygame.time.Clock()
-
-# testing
-cards[50].rect.x, cards[50].rect.y = 700, 500
-cards[24].rect.x, cards[24].rect.y = 300, 200
-cards[5].rect.x, cards[5].rect.y = 200, 500
-
-# pile test
-pile = [cards[50], cards[5], cards[24]]
 
 # main game loop
 while True:
@@ -107,18 +129,20 @@ while True:
         if event.type == pygame.QUIT: 
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            for card in pile:
-                if card.draggable: 
-                    card.handleMouseDown()
+            for pile in piles:
+                for card in pile:
+                    if card.draggable: 
+                        card.handleMouseDown()
         if event.type == pygame.MOUSEMOTION and Card.held:
             Card.held.handleMouseMotion()
         if event.type == pygame.MOUSEBUTTONUP and Card.held:
-            Card.held.handleMouseUp(pile)
+            Card.held.handleMouseUp()
 
     # Draw all assets
     screen.fill(darkGreen)
-    for card in pile:
-        card.draw(screen)
+    for pile in piles:
+        for card in pile:
+            card.draw(screen)
     pygame.display.flip()
 
     clock.tick(60)
