f8b1c0fbe8666bd839ecc788336a2753ab6b71b0
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2021-06-26 19:05:36 +0100

    moving pile to return to original pile added

--- Solitaire.py
+++ Solitaire.py
@@ -14,92 +14,113 @@ def loadImage(path, newSize=None):
         image = pygame.transform.scale(image, newSize)
     return image
 
-# card class containing image, position, and game data
+# card class containing image, position, size data
 class Card:
-    # keep track of which card is held by the cursor and previous mouse position
-    held = None
-    prevMouseX = 0
-    prevMouseY = 0
+    # set global card attributes
+    size = width, height = 95, 140
+    imagePath = "PNG-cards"
+    suits = ("clubs", "diamonds", "hearts", "spades")
 
-    # cardback image
-    cardback = None
+    # set cardback image
+    cardback = loadImage(f"{imagePath}/cardback.png", size)
 
-    def __init__(self, number, suit, image):
+    def __init__(self, number, suit):
         self.number = number
         self.suit = suit
-        self.image = image
-        self.rect = image.get_rect()
+        self.image = loadImage(f"{Card.imagePath}/{number}_of_{suit}.png", Card.size)
+        self.rect = self.image.get_rect()
         self.faceUp = True
         self.draggable = True
-
-    def handleMouseDown(self):
-        # check is cursor is inside card
-        mouseX, mouseY = pygame.mouse.get_pos()
-        # assume true to begin with
-        isInside = True
-
-        if mouseX < self.rect.x or mouseX > self.rect.x + self.rect.w:
-            isInside = False
-        elif mouseY < self.rect.y or mouseY > self.rect.y + self.rect.h: 
-            isInside = False
-
-        # set held card to this card if cursor is inside
-        if isInside: 
-            Card.held = self
-            Card.prevMouseX = mouseX
-            Card.prevMouseY = mouseY
-
-    def handleMouseMotion(self):
-        # move card with cursor if held
-        mouseX, mouseY = pygame.mouse.get_pos()
-        # move card to new position based on previous and current mouse position
-        self.rect.x = self.rect.x + mouseX - Card.prevMouseX  
-        self.rect.y = self.rect.y + mouseY - Card.prevMouseY
-        Card.prevMouseX = mouseX
-        Card.prevMouseY = mouseY 
-
-    def handleMouseUp(self):
-        Card.held = None
-
+        
     def draw(self, screen):
         # draws image onto rect surface
         screen.blit(self.image, self.rect)
 
-
+# pile class containing cards
 class Pile:
+    # pile spacing for where to place them on the screen
+    cardSpacing = 36
+    pileSpacing = 120
+
+    def __init__(self, pile):
+        self.pile = pile 
+        
+    def handleMouseDown(self, movingPile):
+        # check is cursor is inside card
+        mouseX, mouseY = pygame.mouse.get_pos()
+
+        # check if cursor is inside any of the cards in the pile
+        for index, card in reversed(list(enumerate(self.pile))):
+            # assume true to begin with
+            inside = True
+
+            if mouseX < card.rect.x or mouseX > card.rect.x + card.rect.w:
+                inside = False
+            elif mouseY < card.rect.y or mouseY > card.rect.y + card.rect.h: 
+                inside = False
+
+            if inside: 
+                # partition pile into moving pile
+                movingPile.pile = self.pile[index:]
+                self.pile = self.pile[:index]
+                movingPile.previousPile = self.pile
+                
+                movingPile.prevMouseX = mouseX
+                movingPile.prevMouseY = mouseY
+                break
+
+    def draw(self, screen):
+        for card in self.pile:
+            card.draw(screen)
+
+
+class MovingPile(Pile):
+# When pile is being dragged by cursor
 
     def __init__(self):
-        pass
+        Pile.__init__(self, pile=[])
+        self.prevMouseX = 0
+        self.prevMouseY = 0
+        self.previousPile = None
 
+    def handleMouseMotion(self):
+        # move card with cursor if held
+        mouseX, mouseY = pygame.mouse.get_pos()
+
+        for card in self.pile:
+            # move card to new position based on previous and current mouse position
+            card.rect.x += mouseX - self.prevMouseX  
+            card.rect.y += mouseY - self.prevMouseY
+
+        self.prevMouseX = mouseX
+        self.prevMouseY = mouseY
+
+    def handleMouseUp(self):
+        
+        # if no new pile selected, return moving pile to previous pile
+        for index, card in enumerate(self.pile):
+            card.rect.x = self.previousPile[-1].rect.x
+            card.rect.y = self.previousPile[-1].rect.y + Pile.cardSpacing
+            self.previousPile.append(card)
+
+        # reset held card/pile held
+        self.pile = []
 
 # set screen properties
 screenSize = width, height = 1100, 800
 darkGreen = 50, 122, 14 # for background
 
-# set card properties
-cardSize = width, height = 95, 140
-imagePath = "PNG-cards"
-suits = ("clubs", "diamonds", "hearts", "spades")
-deck = []
-
 # create deck
-for suit in suits:
+deck = []
+for suit in Card.suits:
     # 1 = ace, 11 = jack, 12 = queen, 13 = king
     for number in range(1, 14):
-        image = loadImage(f"{imagePath}/{number}_of_{suit}.png", cardSize)
-        deck.append(Card(number, suit, image))
-
-# set cardback image
-Card.cardback = loadImage(f"{imagePath}/cardback.png", cardSize)
+        deck.append(Card(number, suit))
 
 # shuffle deck
 random.shuffle(deck)
 
-# remove cards from deck and add to piles
-
-# create 7 piles
-cardSpacing = 36
-pileSpacing = 120
+# create 7 piles from shuffled deck
 piles = []
 startingCardPosition = [100, 100]
 cardPosition = startingCardPosition.copy()
@@ -110,13 +131,16 @@ for _ in range(7):
         card.rect.x, card.rect.y = cardPosition
         pile.append(card)
         # move position of next card in the y direction
-        cardPosition[1] += cardSpacing 
-    
-    piles.append(pile)
+        cardPosition[1] += Pile.cardSpacing 
+
+    piles.append(Pile(pile))
     # move position of next pile in the x direction
-    cardPosition[0] += pileSpacing
+    cardPosition[0] += Pile.pileSpacing
     # reset y position of pile
     cardPosition[1] = startingCardPosition[1]
+
+# create a moving pile (intially empty)
+movingPile = MovingPile()
 
 # set screen and clock
 screen = pygame.display.set_mode(screenSize)
@@ -128,22 +152,24 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             sys.exit()
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             for pile in piles:
-                for card in pile:
-                    if card.draggable: 
-                        card.handleMouseDown()
-        if event.type == pygame.MOUSEMOTION and Card.held:
-            Card.held.handleMouseMotion()
-        if event.type == pygame.MOUSEBUTTONUP and Card.held:
-            Card.held.handleMouseUp()
+                pile.handleMouseDown(movingPile)
 
-    # Draw all assets
+        if event.type == pygame.MOUSEMOTION and movingPile.pile:
+            movingPile.handleMouseMotion()
+
+        if event.type == pygame.MOUSEBUTTONUP and movingPile.pile:
+            movingPile.handleMouseUp()
+
+    # render
     screen.fill(darkGreen)
     for pile in piles:
-        for card in pile:
-            card.draw(screen)
+        pile.draw(screen)
+    movingPile.draw(screen)
     pygame.display.flip()
 
+    # maintain 60 fps
     clock.tick(60)
 
