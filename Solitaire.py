cd4a4a411e8d573dc7744388229544d848bb1686
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2021-06-27 19:22:45 +0100

    stacking problem fixed

--- Solitaire.py
+++ Solitaire.py
@@ -14,7 +14,7 @@ def loadImage(path, newSize=None):
         image = pygame.transform.scale(image, newSize)
     return image
 
-# card class containing image, position, size data
+# card class containing image, position, and size data
 class Card:
     # set global card attributes
     size = width, height = 95, 140
@@ -42,32 +42,16 @@ class Pile:
     cardSpacing = 36
     pileSpacing = 120
 
-    def __init__(self, pile):
+    def __init__(self, pile, posX, posY):
+        self.posX = posX
+        self.posY = posY
         self.pile = pile 
-        
-    def handleMouseDown(self, movingPile):
-        # check is cursor is inside card
-        mouseX, mouseY = pygame.mouse.get_pos()
 
-        # check if cursor is inside any of the cards in the pile
-        for index, card in reversed(list(enumerate(self.pile))):
-            # assume true to begin with
-            inside = True
-
-            if mouseX < card.rect.x or mouseX > card.rect.x + card.rect.w:
-                inside = False
-            elif mouseY < card.rect.y or mouseY > card.rect.y + card.rect.h: 
-                inside = False
-
-            if inside: 
-                # partition pile into moving pile
-                movingPile.pile = self.pile[index:]
-                self.pile = self.pile[:index]
-                movingPile.previousPile = self.pile
-                
-                movingPile.prevMouseX = mouseX
-                movingPile.prevMouseY = mouseY
-                break
+    def update(self):
+        # update positions of the cards based on the first card
+        for index, card in enumerate(self.pile):
+            card.rect.x = self.posX
+            card.rect.y = self.posY + Pile.cardSpacing * index
 
     def draw(self, screen):
         for card in self.pile:
@@ -78,10 +62,28 @@ class MovingPile(Pile):
 # When pile is being dragged by cursor
 
     def __init__(self):
-        Pile.__init__(self, pile=[])
+        Pile.__init__(self, pile=[], posX=0, posY=0)
         self.prevMouseX = 0
         self.prevMouseY = 0
         self.previousPile = None
+
+    def handleMouseDown(self, pile):
+        # check is cursor is inside card
+        mouseX, mouseY = pygame.mouse.get_pos()
+
+        # check if cursor is inside any of the cards in the pile
+        for index, card in reversed(list(enumerate(pile.pile))):
+            
+            # if mouse is inside card
+            if card.rect.collidepoint(mouseX, mouseY): 
+                # partition pile into moving pile
+                self.pile = pile.pile[index:]
+                pile.pile = pile.pile[:index]
+                self.previousPile = pile
+                
+                self.prevMouseX = mouseX
+                self.prevMouseY = mouseY
+                return
 
     def handleMouseMotion(self):
         # move card with cursor if held
@@ -89,22 +91,18 @@ class MovingPile(Pile):
 
         for card in self.pile:
             # move card to new position based on previous and current mouse position
-            card.rect.x += mouseX - self.prevMouseX  
-            card.rect.y += mouseY - self.prevMouseY
+            card.rect.move_ip(mouseX - self.prevMouseX, mouseY - self.prevMouseY)
 
         self.prevMouseX = mouseX
         self.prevMouseY = mouseY
 
     def handleMouseUp(self):
-        
         # if no new pile selected, return moving pile to previous pile
-        for index, card in enumerate(self.pile):
-            card.rect.x = self.previousPile[-1].rect.x
-            card.rect.y = self.previousPile[-1].rect.y + Pile.cardSpacing
-            self.previousPile.append(card)
-
+        self.previousPile.pile.extend(self.pile)
+        self.previousPile.update()
+        
         # reset held card/pile held
-        self.pile = []
+        self.pile.clear()
 
 # set screen properties
 screenSize = width, height = 1100, 800
@@ -122,22 +120,20 @@ random.shuffle(deck)
 
 # create 7 piles from shuffled deck
 piles = []
-startingCardPosition = [100, 100]
-cardPosition = startingCardPosition.copy()
+pilePosX = 100
+pilePosY = 100
 for _ in range(7):
-    pile = []
+    newPile = Pile([], pilePosX, pilePosY)
     for _ in range(5):
         card = deck.pop()
-        card.rect.x, card.rect.y = cardPosition
-        pile.append(card)
-        # move position of next card in the y direction
-        cardPosition[1] += Pile.cardSpacing 
+        newPile.pile.append(card)
 
-    piles.append(Pile(pile))
+    # update positions of cards based on position of the pile
+    newPile.update()
+    piles.append(newPile)
+
     # move position of next pile in the x direction
-    cardPosition[0] += Pile.pileSpacing
-    # reset y position of pile
-    cardPosition[1] = startingCardPosition[1]
+    pilePosX += Pile.pileSpacing
 
 # create a moving pile (intially empty)
 movingPile = MovingPile()
@@ -151,11 +147,12 @@ while True:
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
+            pygame.quit()
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             for pile in piles:
-                pile.handleMouseDown(movingPile)
+                movingPile.handleMouseDown(pile)
 
         if event.type == pygame.MOUSEMOTION and movingPile.pile:
             movingPile.handleMouseMotion()
