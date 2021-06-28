2ac6cf6df7830d6415b1a1acf372262354e37da7
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2021-06-28 01:59:58 +0100

    added stock and waste piles

--- Solitaire.py
+++ Solitaire.py
@@ -28,11 +28,31 @@ class Card:
     def __init__(self, number, suit):
         self.number = number
         self.suit = suit
+        self.colour = Card.getColour(suit)
         self.image = loadImage(f"{Card.imagePath}/{number}_of_{suit}.png", Card.size)
         self.rect = self.image.get_rect()
         self.faceUp = False
-        self.draggable = False
         
+    @staticmethod
+    def getColour(suit):
+        if suit == "clubs" or suit == "spades":
+            return "black"
+        return "red"
+
+    @staticmethod    
+    def oppositeColour(card1, card2):
+        # return true if the two cards have opposite colours
+        if card1.colour == card2.colour:
+            return False
+        return True
+
+    @staticmethod
+    def validNumber(card1, card2):
+        # return true if card 2 is valued 1 less than card 1
+        if card2.number == card1.number - 1:
+            return True
+        return False
+
     def draw(self, screen):
         # draws image or cardback depending if face up or not
         image = self.cardbackImage
@@ -46,7 +66,10 @@ class Pile:
     cardSpacing = 36
     pileSpacing = 140
 
-    def __init__(self, pile, posX, posY):
+    # set empty pile slot
+    emptyPileSlotImage = loadImage(f"{Card.imagePath}/empty_pile_slot.png", Card.size)
+
+    def __init__(self, pile=[], posX=0, posY=0):
         self.posX = posX
         self.posY = posY
         self.pile = pile 
@@ -58,14 +81,22 @@ class Pile:
             card.rect.y = self.posY + Pile.cardSpacing * index
 
     def draw(self, screen):
-        for card in self.pile:
-            card.draw(screen)
+        # if pile is exists
+        if self.pile:
+            # draw cards
+            for card in self.pile:
+                card.draw(screen)
+        else: 
+            # draw empty pile slot
+            rect = pygame.Rect(self.posX, self.posY, Card.size[0], Card.size[1])
+            screen.blit(Pile.emptyPileSlotImage, rect)
+
 
 # When pile is being dragged by cursor
 class MovingPile(Pile):
     # inherit pile class
     def __init__(self):
-        Pile.__init__(self, pile=[], posX=0, posY=0)
+        Pile.__init__(self)
         # keep track of mouse positions
         self.prevMouseX = 0
         self.prevMouseY = 0
@@ -73,19 +104,19 @@ class MovingPile(Pile):
         self.previousPile = None
 
     def handleMouseDown(self, pile):
-        # check is cursor is inside card
+        # get current mouse position
         mouseX, mouseY = pygame.mouse.get_pos()
 
         # check if cursor is inside any of the cards in the pile (starting from last card)
         for index, card in reversed(list(enumerate(pile.pile))):
             # if mouse is inside card
-            if card.rect.collidepoint(mouseX, mouseY) and card.draggable: 
+            if card.rect.collidepoint(mouseX, mouseY) and card.faceUp: 
                 # partition pile into moving pile
                 self.pile = pile.pile[index:]
                 pile.pile = pile.pile[:index]
                 self.previousPile = pile
                 
-                # set moving pile position to the same as the card
+                # set moving pile position to the card
                 self.posX = card.rect.x
                 self.posY = card.rect.y
 
@@ -107,13 +138,89 @@ class MovingPile(Pile):
         self.prevMouseX = mouseX
         self.prevMouseY = mouseY
 
-    def handleMouseUp(self):
-        # if no new pile selected, return moving pile to previous pile
-        self.previousPile.pile.extend(self.pile)
-        self.previousPile.update()
+    def handleMouseUp(self, piles):
+        # if no pile selected, return moving pile to previous 
+        pileSelected = False
+        
+        # check if valid pile
+        for pile in piles: 
+            if pile.pile:
+                # check if pile overlaps with card
+                if self.pile[0].rect.colliderect(pile.pile[-1].rect):
+                    # check if opposite colours
+                    if Card.oppositeColour(self.pile[0], pile.pile[-1]):
+                        # check if last card of stationary pile is valued 1 more
+                        if Card.validNumber(pile.pile[-1], self.pile[0]):
+                            # Extend the stationary pile with the moving pile
+                            pile.pile.extend(self.pile)
+                            pile.update()
+                            pileSelected = True
+
+                            # if there is a pile in the previous pile
+                            if self.previousPile.pile:
+                                # flip the last card face up
+                                self.previousPile.pile[-1].faceUp = True
+
+                            break
+
+        if not pileSelected:
+            # return the moving pile to the previous pile
+            self.previousPile.pile.extend(self.pile)
+            self.previousPile.update()
         
         # clear held card/pile held
         self.pile.clear()
+
+    def draw(self, screen):
+        # create seperate draw method to avoid drawing empty card slot
+        if self.pile:
+            Pile.draw(self, screen)
+
+# Contains the remaining cards after setting up the tableau
+class StockPile(Pile):
+
+    def update(self):
+        # update positions of the cards
+        for card in self.pile:
+            card.faceUp = False
+            card.rect.x = self.posX
+            card.rect.y = self.posY
+
+# Contains the card(s) pulled from the stock
+class WastePile(Pile):
+
+    def handleMouseDown(self, stockPile):
+        # get mouse position
+        mouseX, mouseY = pygame.mouse.get_pos()
+        
+        if stockPile.pile:
+            if stockPile.pile[-1].rect.collidepoint(mouseX, mouseY): 
+                # move top card into waste pile
+                self.pile.append(stockPile.pile.pop())
+                self.pile[-1].faceUp = True
+                self.update()
+
+                # track position of mouse
+                self.prevMouseX = mouseX
+                self.prevMouseY = mouseY
+        else:
+            # return waste pile to stock pile
+            self.pile.reverse()
+            stockPile.pile = self.pile.copy()
+            self.pile.clear()
+
+            stockPile.update()
+            self.update()
+
+    def update(self):
+        # update positions of the cards
+        for card in self.pile:
+            card.rect.x = self.posX
+            card.rect.y = self.posY
+
+
+class FoundationPile(Pile):
+    pass
 
 # ------------------------set screen properties--------------------------#
 screenSize = width, height = 1100, 800
@@ -150,7 +257,6 @@ for pileNumber in range(numberOfPiles):
         # turn card face up if card number is greater or equal to pile number
         if cardNumber >= pileNumber:
             card.faceUp = True
-            card.draggable = True
         
         newPile.pile.append(card)
 
@@ -167,6 +273,13 @@ for pileNumber in range(numberOfPiles):
 # create a moving pile (intially empty)
 movingPile = MovingPile()
 
+# place remining cards in stock pile
+stockPile = StockPile(deck)
+stockPile.update()
+
+# create a pile to place cards pulled from the stock (initially empty)
+wastePile = WastePile([], 0, 200)
+
 # set screen and clock
 screen = pygame.display.set_mode(screenSize)
 clock = pygame.time.Clock()
@@ -180,19 +293,23 @@ while True:
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
+            wastePile.handleMouseDown(stockPile)
             for pile in piles:
                 movingPile.handleMouseDown(pile)
+            movingPile.handleMouseDown(wastePile)
 
         if event.type == pygame.MOUSEMOTION and movingPile.pile:
             movingPile.handleMouseMotion()
 
         if event.type == pygame.MOUSEBUTTONUP and movingPile.pile:
-            movingPile.handleMouseUp()
+            movingPile.handleMouseUp(piles)
 
     # render
     screen.fill(darkGreen)
     for pile in piles:
         pile.draw(screen)
+    stockPile.draw(screen)
+    wastePile.draw(screen)
     movingPile.draw(screen)
     pygame.display.flip()
 
